2ded1e5aa3cad96c3bcfb316a7248e59ebbc5fe5
杨一帆 <14608190+YFstudio0224@user.noreply.gitee.com>
2024-07-30 11:54:26 +0800

    实现的decoder模块

--- model.py
+++ model.py
@@ -66,9 +66,9 @@ class AddNorm(nn.Module):
 class Pos_FNN(nn.Module):
     def __init__(self):
         super(Pos_FNN, self).__init__()
-        self.lin_1 = nn.Linear(24, 48)
+        self.lin_1 = nn.Linear(24, 48, bias=False)
         self.relu_1 = nn.ReLU()
-        self.lin_2 = nn.Linear(48, 24)
+        self.lin_2 = nn.Linear(48, 24, bias=False)
         self.relu_2 = nn.ReLU()
 
     def forward(self, X):
@@ -107,6 +107,59 @@ class Encoder(nn.Module):
         for encoder_blk in self.encoder_blks:
             X = encoder_blk(X)
         return X
+
+class CrossAttention_block(nn.Module):
+    def __init__(self):
+        super(CrossAttention_block, self).__init__()
+        self.Wq = nn.Linear(24, 24, bias=False)
+        self.Wk = nn.Linear(24, 24, bias=False)
+        self.Wv = nn.Linear(24, 24, bias=False)
+        self.Wo = nn.Linear(24, 24, bias=False)
+        #里面的qkv均为可学习参数
+
+    def forward(self, X, X_en):
+        Q, K, V = self.Wq(X), self.Wk(X_en), self.Wv(X_en)
+        Q, K, V = transpose_qkv(Q), transpose_qkv(K), transpose_qkv(V)
+        O = attention(Q, K, V)
+        O = transpost_o(O)
+        O = self.Wo(O)
+        return O
+
+class Decoder_blk(nn.Module):
+    def __init__(self):
+        super(Decoder_blk, self).__init__()
+        self.attention = Attention_block()
+        self.add_norm_1 = AddNorm()
+        self.cross_attention = CrossAttention_block()#这里的多头注意力跟attention_block不一样，接收的不止有ebd之后的X，还有编码器编码后的X_en
+        self.add_norm_2 = AddNorm()
+        self.FNN = Pos_FNN()
+        self.add_norm_3 = AddNorm()
+
+    def forward(self, X, X_en):
+        X_1 = self.attention(X)
+        X = self.add_norm_1(X, X_1)
+        X_1 = self.cross_attention(X, X_en)
+        X = self.add_norm_2(X, X_1)
+        X_1 = self.FNN(X)
+        X = self.add_norm_3(X_1)
+        return X
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        self.ebd = EBD()
+        self.decoder_blks = nn.Sequential()
+        self.decoder_blks.append(Decoder_blk())
+        self.decoder_blks.append(Decoder_blk())
+        self.dense = nn.Linear(24, 28, bias=False)
+
+    def forward(self, X, X_en):
+        X = self.ebd(X)
+        for layer in self.decoder_blks:
+            X = layer(X, X_en)
+        return X
+
+
 
 if __name__ == "__main__" :
     a = torch.ones((2,12)).long()
